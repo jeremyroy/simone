b2a6b8b3cc47b69000ec389c8eb3042ca1abca4c
Jeremy Roy <jeremy.roy@queensu.ca>
2017-10-21 01:12:15 -0400

    Created gradle package 'simone' and project 'phone_imu'

---
+++ phone_imu/src/main/java/com/github/jeremyroy/simone/phone_imu/PhoneImu.java
@@ -0,0 +1,15 @@
+package com.github.jeremyroy.simone.phone_imu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneImu extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
