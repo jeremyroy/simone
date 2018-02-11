fa445c06c565e160dfab721b088cec0ed411911e
Jeremy Roy <jeremy.roy@queensu.ca>
2018-02-10 22:31:48 -0500

    Motor enable/disable using service

--- phone_imu/src/main/java/com/github/jeremyroy/simone/phone_imu/MotorNode.java
+++ phone_imu/src/main/java/com/github/jeremyroy/simone/phone_imu/MotorNode.java
@@ -8,24 +8,31 @@ import org.ros.node.ConnectedNode;
 import org.ros.node.Node;
 import org.ros.message.MessageListener;
 import org.ros.node.topic.Subscriber;
+import org.ros.node.service.ServiceServer;
+import org.ros.node.service.ServiceResponseBuilder;
 
 import simone_msgs.MotorCTRL;
+import hector_uav_msgs.EnableMotors;
+import hector_uav_msgs.EnableMotorsRequest;
+import hector_uav_msgs.EnableMotorsResponse;
 
 public class MotorNode extends AbstractNodeMain
 {
     private final AudioManager m_audio_manager;
 
     private String topic_name;
+    private String motor_service_name;
 
     private Motors m_motors;
 
     public MotorNode(AudioManager am) {
-        this(am, "phone_imu");
+        this(am, "motor_node", "enable_motors");
     }
 
-    public MotorNode(AudioManager am, String topic) {
+    public MotorNode(AudioManager am, String topic, String motor_service) {
         /* Set up topic */
         this.topic_name = topic;
+        this.motor_service_name = motor_service;
 
         /* Set up the motors object */
         // Get the recommended sample rate from the phone
@@ -44,25 +51,39 @@ public class MotorNode extends AbstractNodeMain
 
     @Override
     public void onStart(ConnectedNode connectedNode) {
-        Subscriber<simone_msgs.MotorCTRL> subscriber = connectedNode.newSubscriber("motor_node", simone_msgs.MotorCTRL._TYPE);
+        Subscriber<simone_msgs.MotorCTRL> subscriber =
+                connectedNode.newSubscriber(this.topic_name, simone_msgs.MotorCTRL._TYPE);
         subscriber.addMessageListener(new MessageListener<simone_msgs.MotorCTRL>() {
             @Override
             public void onNewMessage(simone_msgs.MotorCTRL message){
-                if (message.getEnableMotors()) {
-                    if (!m_motors.is_enabled())
-                    {
-                        m_motors.resume_motors();
-                    }
+                if (m_motors.is_enabled()) {
                     m_motors.setMotorDuty(Motors.MOTOR_1, message.getM1());
                     m_motors.setMotorDuty(Motors.MOTOR_2, message.getM2());
                     m_motors.setMotorDuty(Motors.MOTOR_3, message.getM3());
                     m_motors.setMotorDuty(Motors.MOTOR_4, message.getM4());
                 }
-                else if (m_motors.is_enabled())                {
-                    m_motors.pause_motors();
-                }
             }
         });
+
+        ServiceServer<EnableMotorsRequest, EnableMotorsResponse> server =
+                connectedNode.newServiceServer(this.motor_service_name, EnableMotors._TYPE,
+                        new ServiceResponseBuilder<EnableMotorsRequest, EnableMotorsResponse>() {
+                            @Override
+                            public void build(EnableMotorsRequest request, EnableMotorsResponse response)
+                            {
+                                if (request.getEnable() && !m_motors.is_enabled())
+                                {
+                                    m_motors.resume_motors();
+                                }
+                                else if (m_motors.is_enabled())
+                                {
+                                    m_motors.pause_motors();
+                                }
+
+                                response.setSuccess(true);
+                            }
+                        });
+
     }
 
     @Override
